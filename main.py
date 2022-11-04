f7737a164b7e2e4568f4abe07b3498536eec9183
Mauritshidde <Maurits.diepeveen@coderclass.nl>
2022-11-04 13:59:52 +0100

    meer bestanden
    d

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-
-Cw = 5
-def calc_luchtweerstand(v, Cw, A, T, F): #per meter
-    # p = airdensity, M = molaire massa lucht, R = gasconstante
-    Tper_m = 9.8 / 1000
-    
-    T = T - Tper_m
-    G = 6.67384 * 10^-11
-    M = 28.97 # constant
-    R = 8.314472 # constant
-    p = p * exp(-M*G*H/(R*T))
-    Rho = (p * M) / (R * T)
-    k = 0.5 * Cw * A * Rho
-    Fw = k * v * v
-
-    return Fw
-
-
-
-
-
+from Fw import *
 
 def main():
     notLanded = True
