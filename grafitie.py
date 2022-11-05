99787b3461869abee325266531519681c394557b
Twan Reijinga <twan.reijinga@coderclass.nl>
2022-11-05 19:38:05 +0100

    calculate grafitie force

---
+++ grafitie.py
@@ -0,0 +1,12 @@
+from math import pow
+
+
+def calc_grafitatiekracht(height, MASS_ROCKET):
+    GRAFITATIONAL_CONSTANT = 6.67384 * pow(10, -11)  # Nm^2/kg^2
+    RADIUS_EARTH = 6.371 * pow(10, 6)  # m
+    MASS_EARTH = 5.972 * pow(10, 24)  # kg
+
+    FORCE_GRAFITATIE = GRAFITATIONAL_CONSTANT * \
+        (MASS_EARTH * MASS_ROCKET) / (RADIUS_EARTH + height)
+
+    return FORCE_GRAFITATIE
