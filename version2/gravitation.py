1988b02f4fedf01ff6d41f0f1dfa5fc2491f4544
Twan Reijinga <43951307+Twan-Reijinga@users.noreply.github.com>
2022-11-05 22:51:26 +0100

    add air resistance and temp calculation

--- version2/gravitation.py
+++ version2/gravitation.py
@@ -2,11 +2,11 @@ from math import pow
 
 
 def calc_gravitationforce(height, MASS_ROCKET):
-    GRAFITATIONAL_CONSTANT = 6.67384 * pow(10, -11)  # Nm^2/kg^2
+    GRAVITATIONAL_CONSTANT = 6.67384 * pow(10, -11)  # Nm^2/kg^2
     RADIUS_EARTH = 6.371 * pow(10, 6)  # m
     MASS_EARTH = 5.972 * pow(10, 24)  # kg
 
-    FORCE_GRAVITATION = GRAFITATIONAL_CONSTANT * \
+    FORCE_GRAVITATION = GRAVITATIONAL_CONSTANT * \
         MASS_EARTH * MASS_ROCKET / pow(RADIUS_EARTH + height, 2) # N
 
     return FORCE_GRAVITATION
---
+++ version2/temperature.py
@@ -0,0 +1,3 @@
+def calc_temperature(height):
+  GROUND_TEMPERATURE = 298 # K
+  return GROUND_TEMPERATURE - height * 0.0065
