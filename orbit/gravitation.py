69917f809ce5006e1358b319ae525b9be6f0381a
Twan Reijinga <43951307+Twan-Reijinga@users.noreply.github.com>
2022-11-08 17:56:54 +0100

    divide project in 3 parts: langing, launch and orbit

---
+++ orbit/gravitation.py
@@ -0,0 +1,12 @@
+from math import pow
+
+
+def calc_gravitationforce(height, MASS_ROCKET):
+    GRAVITATIONAL_CONSTANT = 6.67384 * pow(10, -11)  # Nm^2/kg^2
+    RADIUS_EARTH = 6.371 * pow(10, 6)  # m
+    MASS_EARTH = 5.972 * pow(10, 24)  # kg
+
+    FORCE_GRAVITATION = GRAVITATIONAL_CONSTANT * \
+        MASS_EARTH * MASS_ROCKET / pow(RADIUS_EARTH + height, 2)  # N
+
+    return FORCE_GRAVITATION
---
+++ orbit/main.py
@@ -0,0 +1,34 @@
+from math import pi
+from gravitation import calc_gravitationforce
+from airresistance import calc_airresistance
+from plot import *
+
+def main():
+    t = 0  # s
+    dt = 1  # s
+    iterations = 10  # times
+    height = 30000  # m
+    BURN_RATE = 1529.63  # kg / s
+    velocity = 0  # m/s
+    mass_rocket = 549054  # kg (mass falcon 9)
+    force_thrust = 7607000  # N (thrust falcon 9 in vacuum)
+    TEMPERATURE = 298  # K
+    area = 2 * pi * 3.7  # m^2
+    for i in range(iterations):
+        t += dt
+        force_gravitation = calc_gravitationforce(
+            height, mass_rocket)  # Nm^2/kg^2
+        force_resistance = calc_airresistance(
+            height, TEMPERATURE, area, velocity)
+        force_netto = force_resistance + force_thrust - force_gravitation
+        velocity += force_netto / mass_rocket * dt
+        height += velocity * dt
+
+        print()
+        print("--- iteration ", i, " ---")
+        print('graviatation:', force_gravitation)
+        print('resistance:', force_resistance)
+        print()
+    
+
+main()
