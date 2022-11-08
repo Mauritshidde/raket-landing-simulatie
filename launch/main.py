5f0d7aaf4a2b4c6e516b764af87f4a924371090b
Mauritshidde <Maurits.diepeveen@coderclass.nl>
2022-11-08 21:53:48 +0100

    werkende launch
    launch werkt met een if statement die gaat tot een ingestelde max hoogte in dit geval 200 km = 200000 m
    en plot de resultaten in 4 grafieken

--- launch/main.py
+++ launch/main.py
@@ -4,9 +4,16 @@ from airresistance import calc_airresistance
 from plot import *
 
 def main():
+    energie_list = []
+    kracht_list = []
+    tijd_list = []
+    snelheid_list = []
+    height_list = []
+
     t = 0  # s
     dt = 1  # s
-    iterations = 10  # times
+    Rv = 42.8 * pow(10, 6) # Kerosine in J/kg
+    iterations = 100  # times
     height = 30000  # m
     BURN_RATE = 1529.63  # kg / s
     velocity = 0  # m/s
@@ -14,21 +21,50 @@ def main():
     force_thrust = 7607000  # N (thrust falcon 9 in vacuum)
     TEMPERATURE = 298  # K
     area = 2 * pi * 3.7  # m^2
+    force_netto = 0 # N
+    totale_verbruikte_energie = 0
+    target_height = 200000 # m
     for i in range(iterations):
         t += dt
-        force_gravitation = calc_gravitationforce(
-            height, mass_rocket)  # Nm^2/kg^2
-        force_resistance = calc_airresistance(
-            height, TEMPERATURE, area, velocity)
-        force_netto = force_resistance + force_thrust - force_gravitation
-        velocity += force_netto / mass_rocket * dt
-        height += velocity * dt
+        if (height < target_height):
+            force_gravitation = calc_gravitationforce(
+                height, mass_rocket)  # Nm^2/kg^2
+            force_resistance = calc_airresistance(
+                height, TEMPERATURE, area, velocity)
+            
+            if (velocity >= 0):
+                force_netto = force_thrust - force_gravitation - force_resistance
+            else:
+                force_netto = force_resistance + force_thrust - force_gravitation
+                
+            velocity += force_netto / mass_rocket * dt
+            height += velocity * dt
 
+            used_fuel = BURN_RATE * dt
+            chemische_energie = used_fuel * Rv
+            totale_verbruikte_energie += chemische_energie
+
+        else:
+            pass
+
+            
+            
+        energie_list.append(totale_verbruikte_energie)
+        kracht_list.append(force_netto) 
+        tijd_list.append(t) 
+        height_list.append(height)
+        snelheid_list.append(velocity)
+            
         print()
         print("--- iteration ", i, " ---")
         print('graviatation:', force_gravitation)
         print('resistance:', force_resistance)
-        print()
+        print(height)
+
+    all_y_list = [energie_list, kracht_list, snelheid_list, height_list]
+    all_x_list = [tijd_list, tijd_list, tijd_list, tijd_list]
+
+    plot_values(["energie in Joule", "kracht in Newton", "snelheid in m/s"], ["tijd in s", "tijd in s", "tijd in s"], all_x_list, all_y_list)
     
 
 main()
--- launch/plot.py
+++ launch/plot.py
@@ -1,14 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
-def plot_values(xlabel, ylabel, xvalues, yvalues):
-    xpoints = np.array(xvalues)
-    ypoints = np.array(yvalues)
+def plot_values(xlabels, ylabels, xvalues, yvalues):
+    # xpoints = np.array(xvalues)
+    # ypoints = np.array(yvalues)
 
-    plt.plot(xpoints, ypoints)
+    # plt.plot(xpoints, ypoints)
 
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    # plt.xlabel(xlabel)
+    # plt.ylabel(ylabel)
+    
+    figure, axis = plt.subplots(2, 2)
+
+    # For Sine Function
+    # for i in range(len(xvalues)):
+    #     xpoints = np.array(xvalues[i])
+    #     ypoints = np.array(yvalues[i])
+    #     axis[0, i].plot(xpoints, ypoints)
+    #     axis[0, i].set_title("graph")
+    #     # axis[0, 0].xlabel(xlabels[i])
+    #     # axis[0, 0].ylabel(ylabels[i])
+
+    xpoints = np.array(xvalues[0])
+    ypoints = np.array(yvalues[0])
+    axis[0, 1].plot(xpoints, ypoints)
+    axis[0, 1].set_title("energie")
+    xpoints = np.array(xvalues[1])
+    ypoints = np.array(yvalues[1])
+    axis[0, 0].plot(xpoints, ypoints)
+    axis[0, 0].set_title("force")
+    xpoints = np.array(xvalues[2])
+    ypoints = np.array(yvalues[2])
+    axis[1, 0].plot(xpoints, ypoints)
+    axis[1, 0].set_title("speed")
+    xpoints = np.array(xvalues[3])
+    ypoints = np.array(yvalues[3])
+    axis[1, 1].plot(xpoints, ypoints)
+    axis[1, 1].set_title("height")
 
     plt.show()
