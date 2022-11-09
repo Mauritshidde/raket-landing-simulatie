7fa8654f73bf4e1edcab7491a705ee8b22c0676c
Twan Reijinga <twan.reijinga@coderclass.nl>
2022-11-09 13:12:17 +0100

    update airresistnance.py in all folders

--- landing/airresistance.py
+++ landing/airresistance.py
@@ -3,15 +3,20 @@ from math import pow, exp
 
 def calc_airresistance(height, temperature, area, velocity):
     GRAVITATIONAL_CONSTANT = 6.67384 * pow(10, -11)  # Nm^2/kg^2
+    MASS_EARTH = 5.972 * pow(10, 24)  # kg
+    RADIUS_EARTH = 6.371 * pow(10, 6)  # m
     GAS_CONSTANT = 8.314472  # j/K/mol
-    MOLAIR_MASS = 28.97  # g/mol
+    MOLAIR_MASS = 0.02897  # kg/mol
     PRESSURE_ZERO = 1013  # hPa
     Cw = 0.82
+    fall_acceleration = GRAVITATIONAL_CONSTANT * \
+        MASS_EARTH / pow(RADIUS_EARTH + height, 2)  # m/s^2
 
-    pressure_exponent = -MOLAIR_MASS * GRAVITATIONAL_CONSTANT * \
+    pressure_exponent = -MOLAIR_MASS * fall_acceleration * \
         height / (GAS_CONSTANT * temperature)
     pressure = PRESSURE_ZERO * exp(pressure_exponent)  # hPa
     density = (pressure * MOLAIR_MASS) / (GAS_CONSTANT * temperature)  # kg/m^3
+    print(pressure_exponent)
     k = 0.5 * Cw * area * density
     force_airresistance = k * pow(velocity, 2)
     return force_airresistance
