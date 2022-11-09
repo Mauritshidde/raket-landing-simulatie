169f1700083dcf59609c51601108cf7219289278
Twan Reijinga <twan.reijinga@coderclass.nl>
2022-11-09 13:10:56 +0100

    calculate fall acceleration

--- launch/airresistance.py
+++ launch/airresistance.py
@@ -2,14 +2,18 @@ from math import pow, exp
 
 
 def calc_airresistance(height, temperature, area, velocity):
-    valversnelling = 9.81 # m/s^2
-    # GRAVITATIONAL_CONSTANT = 6.67384 * pow(10, -11)  # Nm^2/kg^2
+    GRAVITATIONAL_CONSTANT = 6.67384 * pow(10, -11)  # Nm^2/kg^2
+    MASS_EARTH = 5.972 * pow(10, 24)  # kg
+    RADIUS_EARTH = 6.371 * pow(10, 6)  # m
     GAS_CONSTANT = 8.314472  # j/K/mol
     MOLAIR_MASS = 0.02897  # kg/mol
     PRESSURE_ZERO = 1013  # hPa
     Cw = 0.82
+    fall_acceleration = GRAVITATIONAL_CONSTANT * \
+        MASS_EARTH / pow(RADIUS_EARTH + height, 2)  # m/s^2
 
-    pressure_exponent = -MOLAIR_MASS * valversnelling * height / (GAS_CONSTANT * temperature)
+    pressure_exponent = -MOLAIR_MASS * fall_acceleration * \
+        height / (GAS_CONSTANT * temperature)
     pressure = PRESSURE_ZERO * exp(pressure_exponent)  # hPa
     density = (pressure * MOLAIR_MASS) / (GAS_CONSTANT * temperature)  # kg/m^3
     print(pressure_exponent)
